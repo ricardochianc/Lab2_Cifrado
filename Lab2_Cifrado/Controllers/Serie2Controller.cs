7b935875f0b20ce505f9fd5a587688727fd27c2b
Ricardo Chian <ricardochianc@gmail.com>
2019-10-17 16:00:00 -0600

    Se agregó controlador para la serie 2, faltan las vistas parciales y controlador de SDES

---
+++ Lab2_Cifrado/Controllers/Serie2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2_Cifrado.Controllers
+{
+    public class Serie2Controller : Controller
+    {
+        // GET: Serie2
+        public ActionResult SeleccionarOpcion()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult SeleccionarOpcion(FormCollection formCollection)
+        {
+            if (formCollection["CargarPermutaciones"] != null)
+            {
+                return RedirectToAction("", "");
+            }
+
+            if (formCollection["Continuar"] != null)
+            {
+                return RedirectToAction("", "");
+            }
+
+            return null;
+        }
+    }
+}
