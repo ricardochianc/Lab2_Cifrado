ae702dded44b4178f70a91391a00d1debc99a3dc
Ricardo Chian <ricardochianc@gmail.com>
2019-10-17 15:01:35 -0600

    Cifrado SDES terminado, solo falta conectarlo con MVC

--- BibliotecaDeClases/Cifrado/S-DES/CifradoSDES.cs
+++ BibliotecaDeClases/Cifrado/S-DES/CifradoSDES.cs
@@ -13,7 +13,7 @@ namespace BibliotecaDeClases.Cifrado.S_DES
         private string NombreArchivo { get; set; }
         private string RutaAbsolutaArchivo { get; set; }
         private string RutaAbsolutaServer { get; set; }
-        public string RutaAbsolutaArchivoCif { get; set; } //Para el .scif
+        public string RutaAbsolutaArchivoSCif { get; set; } //Para el .scif
 
         private int Clave { get; set; }
         private SDES_Base UtilidadeSDES { get; set; }
@@ -25,6 +25,7 @@ namespace BibliotecaDeClases.Cifrado.S_DES
             NombreArchivo = nombreArchivo;
             RutaAbsolutaArchivo = RutaAbsArchivo;
             RutaAbsolutaServer = RutaAbsServer;
+            RutaAbsolutaArchivoSCif = "";
             Clave = clave;
 
             UtilidadeSDES = new SDES_Base();
@@ -33,6 +34,10 @@ namespace BibliotecaDeClases.Cifrado.S_DES
 
         public void Cifrar()
         {
+            RutaAbsolutaArchivoSCif = RutaAbsolutaServer + NombreArchivo + ".scif";
+
+            File.Create(RutaAbsolutaArchivo);
+
             var key1 = "";
             var key2 = "";
 
@@ -48,6 +53,8 @@ namespace BibliotecaDeClases.Cifrado.S_DES
                     while (reader.BaseStream.Position != reader.BaseStream.Length)
                     {
                         buffer = reader.ReadBytes(LargoBuffer);
+
+                        var contBuffer = 0;
 
                         foreach (var caracter in buffer)
                         {
@@ -97,19 +104,60 @@ namespace BibliotecaDeClases.Cifrado.S_DES
 
                                 var bloqueIzquierdo2 = "";
                                 var bloqueDerecho2 = "";
+                                var bloqueDerechoOriginal2 = bloqueDerecho2;
 
                                 //Se ingresa el izquierdo en el derecho y derecho en izquierdo con el motivo de intercambiar de lados
                                 UtilidadeSDES.DividirCadenaBits(4,resultadoRonda1, ref bloqueDerecho2,ref bloqueIzquierdo2);
+                                bloqueDerechoOriginal2 = bloqueDerecho2;
 
                                 UtilidadeSDES.AplicarPermutacion("ExpandirPermutar",ref bloqueDerecho2);
 
+                                var binarioResultante2 = UtilidadeSDES.XOR(bloqueDerecho, key2);
+
+                                //Estos serviran para luego hacer las consultas en las SBoxes
+                                var MasIzquierdos2 = (binarioResultante2[0] + binarioResultante2[1] + binarioResultante2[2] + binarioResultante2[3]).ToString();
+                                var MasDerechos2 = (binarioResultante2[4] + binarioResultante2[5] + binarioResultante2[6] + binarioResultante2[7]).ToString();
+
+                                var fila2 = 0;
+                                var columna2 = 0;
+
+                                UtilidadeSDES.ObtenerFilaColumna(MasIzquierdos2, ref fila2, ref columna2);
+
+                                var bitsResultantesSBoxes2 = UtilidadeSDES.ObtenerBitsSBox0(fila2, columna2);
+
+                                UtilidadeSDES.ObtenerFilaColumna(MasDerechos2, ref fila2, ref columna2);
+
+                                bitsResultantesSBoxes2 += UtilidadeSDES.ObtenerBitsSBox1(fila2, columna2);
+
+                                UtilidadeSDES.AplicarPermutacion("P4", ref bitsResultantesSBoxes2); //Se le aplica una permutacion de 4 bits
+
+                                var resultadoBits2 = UtilidadeSDES.XOR(bitsResultantesSBoxes2, bloqueIzquierdo2);
+
+                                var resultadoRonda2 = resultadoBits2.ToString() + bloqueDerechoOriginal2.ToString();
+
+                                UtilidadeSDES.AplicarPermutacion("PInversa",ref resultadoRonda2); //resultadoRonda2 es el que se manda a escribir
+
+                                bufferEscritura[contBuffer] = Convert.ToByte(Convert.ToInt32(resultadoRonda2,2));
                             }
                             else
                             {
                                 throw new Exception("Mayor a 8 bits");
                             }
                         }
+                        //Mandar a escribir buffer de escritura
+                        EscribirBuffer(bufferEscritura);
                     }
+                }
+            }
+        }
+
+        private void EscribirBuffer(byte[] buffer)
+        {
+            using (var file = new FileStream(RutaAbsolutaArchivoSCif,FileMode.Append))
+            {
+                using (var writer = new BinaryWriter(file,Encoding.UTF8))
+                {
+                    writer.Write(buffer);
                 }
             }
         }
